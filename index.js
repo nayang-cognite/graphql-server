e8944c56e2fd74ba7cff8976a180ae42cdb9d9f6
Nancy Yang <nancy.yang@cognite.com>
2021-06-25 08:44:08 -0400

    initial check in for neo4j experiment

--- index.js
+++ index.js
@@ -1,79 +1,31 @@
-const { ApolloServer, gql } = require('apollo-server');
+import { ApolloServer, makeExecutableSchema, makeRemoteExecutableSchema } from 'apollo-server';
+import neo4j from 'neo4j-driver'
+import { makeAugmentedSchema } from 'neo4j-graphql-js';
 
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
-const typeDefs = gql`
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+const typeDefs = `
+type Book {
+   id: ID! @id
+   title: String!
+   author: Author @relation(name: "IS_WRITTEN_BY", direction: IN)
+ }
 
-  # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    title: String
-    author: String
-  }
-
-  type Author {
-    name: String
-  }
-
-  type Query {
-    Books: [Book]
-  }
-
-  type Mutation {
-    addBook(title: String, author: String): Book
-  }
+ type Author {
+   id: ID! @id 
+   name: String!
+   book: Book @relation(name: "WRITE", direction: IN)
+ }
 `;
 
-var books = [
-  {
-    title: 'The Awakening',
-    author: 'Kate Chopin',
-  },
-  {
-    title: 'City of Glass',
-    author: 'Paul Auster',
-  },
-];
+const schema = makeAugmentedSchema({typeDefs: typeDefs})
 
-var authors = [
-   {
-      name: 'Kate Chopin'
-   },
-   {
-      name: 'Paul Auster'
-   }
-]
 
-// Resolvers define the technique for fetching the types defined in the
-// schema. This resolver retrieves books from the "books" array above.
-const resolvers = {
-   Query: {
-      Books: () => books,
-   },
-   Mutation: {
-      addBook: (_, {title, author}) => {
-         titles = books.map(book => book.title)
-         names = authors.map(author => author.name)
-         if (!titles.includes(title)) {
-            books = [...books, {title: title, author: author}]
-            if (!names.includes(author)) {
-               authors = [...authors, {name: author}]
-            }
-            return {
-               title,
-               author
-            }
-         }
-      }
-   }
-};
+const driver = neo4j.driver(
+   'bolt://localhost:7687',
+   neo4j.auth.basic('neo4j', 'test')
+ );
 
-// The ApolloServer constructor requires two parameters: your schema
-// definition and your set of resolvers.
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ schema, context: {driver} });
 
-// The `listen` method launches a web server.
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
 });
